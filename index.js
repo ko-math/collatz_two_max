5b4aecd44aae95226b3cf1d716d9e120d2819f0a
ko-math <sorehakiemasu@gmail.com>
2026-07-14 13:52:12 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const MAX = 100;
+const data = [];
+for(let i = 1;i <= MAX;i++){
+  data.push(col(i));
+}
+
+
+function col(n){
+  let i = n;
+  if(i === 1)return [1,0,1,1];//[n,2^i-1_count,sq,max]
+  const two = 0;
+  const sq = [];
+  let max = i;
+  while(i < n){
+    sq.push(i);
+    if(Math.ceil(Math.log2(i + 1)) === Math.log2(i + 1)two++;
+    if(i > max)max = i;
+    if(i % 2 === 0){
+      i /= 2;
+    } else {
+      i = (3 * i + 1) / 2;
+    }
+  }
+  return [n,two,sq,max];
+}
+
+window.alert(col(5));
